07d2d4e23e24afffc153ca269a83048d75161253
Vignesh Mohan <mvignesh18@Vigneshs-MBP.fios-router.home>
2019-09-04 20:34:38 -0400

    change deploy sh file

--- deploy.sh
+++ deploy.sh
@@ -101,7 +101,7 @@ selectNodeVersion () {
 echo Handling node.js deployment.
 
 # 1. Select node version
-call :SelectNodeVersion
+SelectNodeVersion
 
 # 2. Install npm packages
 IF EXIST "%DEPLOYMENT_SOURCE%\package.json" (
